82b43216d788839a06932267b7ea362a0b807cb4
icancodebeauty <adi@mail.io>
2022-02-01 06:28:39 +0000

    Removed Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
-import Footer from "./pages/Footer";
-import Navbar from "./pages/Navbar";
-import Gallery from "./pages/Gallery";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Gallery />
-      <Footer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
